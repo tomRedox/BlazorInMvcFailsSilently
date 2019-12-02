8a08b9b7ad28fbb45054756209977c00d798e78f
Tom Woodforde <tom.woodforde@redox-software.co.uk>
2019-12-02 16:52:55 +0000

    Add the logging in

--- SSB31Prev2/Data/MethodErrorService.cs
+++ SSB31Prev2/Data/MethodErrorService.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace SSB31Prev2.Data
 {
+    
     public class MethodErrorService
     {
+        public MethodErrorService(ILogger<MethodErrorService> logger)
+        {
+            logger.LogInformation("Test logging");
+        }
+
         public void DoingSomethingThrowsError()
         {
+            
             throw new Exception("Uh oh, method threw an error");
         }
     }
